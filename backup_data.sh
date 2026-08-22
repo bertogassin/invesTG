29cd9d3fc6311a3cb52991b57141759192b62c6b
bertogassin <bertogassin@gmail.com>
2026-08-22 09:53:54 +0000

    Clear repository: remove all tracked files

--- backup_data.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -e
-DIR=/root/resursmap
-STAMP=$(date +%Y%m%d_%H%M%S)
-mkdir -p "$DIR/backups"
-tar -czf "$DIR/backups/data_$STAMP.tar.gz" -C "$DIR" data
-# keep last 20
-ls -1t "$DIR/backups"/data_*.tar.gz 2>/dev/null | tail -n +21 | xargs -r rm -f
-echo "backup ok $STAMP"
