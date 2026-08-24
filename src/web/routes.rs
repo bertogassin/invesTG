cdf548d31a911740a252cc27cad9372afe1ffb87
bertogassin <bertogassin@gmail.com>
2026-08-24 17:55:45 +0000

    8.4B: use explicit handler imports in web routes

--- src/web/routes.rs
+++ src/web/routes.rs
@@ -1,4 +1,15 @@
-use super::handlers::*;
+use super::handlers::{
+    accept_contact_request, add_resource, add_resource_page, admin_approve_resource,
+    admin_bulk_resources, admin_close_report, admin_hide_reported_resource, admin_login,
+    admin_login_page, admin_reject_reported_resource, admin_reject_resource, admin_reports,
+    admin_resources, admin_toggle_active, admin_toggle_premium, admin_toggle_verified,
+    api_contact_request, api_favorite_status, api_favorite_toggle, api_open_count, api_profile_get,
+    api_profile_set, api_report_resource, api_resource_vote, app_auth, app_auth_page, app_cat,
+    app_city, app_continent, app_country, app_me, app_root, app_search, chat_page,
+    contact_requests_page, edit_resource, edit_resource_page, favorites_page, health, home,
+    messages_page, my_resources, notifications_page, public_user_profile, reject_contact_request,
+    resource_profile, send_chat_message,
+};
 use crate::state::app_state::AppState;
 use axum::{
     routing::{get, post},
